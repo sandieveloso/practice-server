2f122c60dddf9656b044c0b0e0daac4f3b3f6b99
Sandie Veloso <sandieveloso@priume.com>
2016-08-10 22:01:35 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+});
+
+// Pass message to client
+
+app.get('api/message', function(req,res){
+	res.send('Get request to the homepage');
+});
+
+// Get message from client
+
+app.post('/api/message', function(req,res){
+	console.log(req.body);
+	res.status(200);
+});
+
+var server = app.listen(5000, function(){
+	console.log('listening on port', server.address().port);
+});
